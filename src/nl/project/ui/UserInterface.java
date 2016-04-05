d716c560d71113ad8f1bb13a07cfa90a0c9e25e6
sanderlo <sanderlooise@gmail.com>
2016-04-05 17:04:01 +0200

    User aangepast

--- src/nl/project/ui/UserInterface.java
+++ src/nl/project/ui/UserInterface.java
@@ -1,9 +1,12 @@
 package nl.project.ui;
 
+import nl.project.team.Team;
+
 public class UserInterface {
 
 	public static void main(String[] args) {
-System.out.println("Hier komt alles!");
+		
+		System.out.println("Hier komt alles!");
 
 	}
 
