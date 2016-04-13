26d4795694ff0d0632aaace314b1e77b670dfd0c
sdebruijn <sijmendeb@gmail.com>
2016-04-14 00:07:04 +0200

    Update UserService.java

--- src/nl/project/service/UserService.java
+++ src/nl/project/service/UserService.java
@@ -1,5 +1,14 @@
 package nl.project.service;
-
-public class UserService {
-
+//package com.websystique.springsecurity.service;
+ 
+import nl.project.user.User;
+ 
+public interface UserService {
+ 
+    void save(User user);
+     
+    User findById(int id);
+     
+    User findBySso(String sso);
+     
 }
