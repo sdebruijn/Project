7ec117d6a4f69fa337ae0d453213c815124735c6
sdebruijn <sijmendeb@gmail.com>
2016-04-14 00:04:44 +0200

    Update UserProfileDao.java

--- src/nl/project/dao/UserProfileDao.java
+++ src/nl/project/dao/UserProfileDao.java
@@ -1,5 +1,15 @@
 package nl.project.dao;
-
-public class UserProfileDao {
-
+//package com.websystique.springsecurity.dao;
+ 
+import java.util.List;
+ 
+import com.websystique.springsecurity.model.UserProfile;
+ 
+public interface UserProfileDao {
+ 
+    List<UserProfile> findAll();
+     
+    UserProfile findByType(String type);
+     
+    UserProfile findById(int id);
 }
