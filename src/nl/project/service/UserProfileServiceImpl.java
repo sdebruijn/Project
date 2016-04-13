2fee15ce0d9e332939501e0c6e470c8a3b7c89d1
sdebruijn <sijmendeb@gmail.com>
2016-04-14 00:06:31 +0200

    Update UserProfileServiceImpl.java

--- src/nl/project/service/UserProfileServiceImpl.java
+++ src/nl/project/service/UserProfileServiceImpl.java
@@ -1,5 +1,31 @@
 package nl.project.service;
-
-public class UserProfileServiceImpl {
-
+//package com.websystique.springsecurity.service;
+ 
+import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+ 
+import nl.project.dao.UserProfileDao;
+import nl.project.user.UserProfile;
+ 
+@Service("userProfileService")
+@Transactional
+public class UserProfileServiceImpl implements UserProfileService{
+     
+    @Autowired
+    UserProfileDao dao;
+     
+    public List<UserProfile> findAll() {
+        return dao.findAll();
+    }
+ 
+    public UserProfile findByType(String type){
+        return dao.findByType(type);
+    }
+ 
+    public UserProfile findById(int id) {
+        return dao.findById(id);
+    }
 }
