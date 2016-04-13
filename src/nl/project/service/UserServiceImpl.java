22ee926580f6dcb84053eb4704b5a0d79848694b
sdebruijn <sijmendeb@gmail.com>
2016-04-14 00:07:42 +0200

    Update UserServiceImpl.java

--- src/nl/project/service/UserServiceImpl.java
+++ src/nl/project/service/UserServiceImpl.java
@@ -1,5 +1,36 @@
 package nl.project.service;
-
-public class UserServiceImpl {
-
+//package com.websystique.springsecurity.service;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+ 
+import nl.project.dao.UserDao;
+import nl.project.user.User;
+ 
+@Service("userService")
+@Transactional
+public class UserServiceImpl implements UserService{
+ 
+    @Autowired
+    private UserDao dao;
+     
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+ 
+     
+    public void save(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        dao.save(user);
+    }
+     
+    public User findById(int id) {
+        return dao.findById(id);
+    }
+ 
+    public User findBySso(String sso) {
+        return dao.findBySSO(sso);
+    }
+     
 }
